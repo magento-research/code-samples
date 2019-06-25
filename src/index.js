77ec26cd8af1fd7448381b793eccac37df0fca31
James Calcaben <calcaben@adobe.com>
2019-06-25 14:16:11 -0500

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-    render() {
-        return <h1>Hello React World!</h1>;
-    }
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
