cfec2827a72f56a023a493f3ae244fbccb0afbf5
James Calcaben <calcaben@adobe.com>
2019-06-25 13:51:07 -0500

    Rename files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/hello-world.js',
+  entry: './src/index.js',
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
