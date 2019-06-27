607367b2319ab450a83ca6c1e9f2ad3d862a97e1
James Calcaben <calcaben@adobe.com>
2019-06-27 09:57:55 -0500

    Directory re-organization

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Home from './home';
 
-import Mask from '../../examples/Mask';
+import Mask from '../../examples/venia/Mask';
 
 const pages = [
   {
